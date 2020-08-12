e05726850879a8dc86861af0492173abb8519e53
Oladapo Ademola <ademolaoladapo@gmail.com>
2020-08-12 20:58:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+movies = []
+
+
+def menu():
+
+    user_input = input("Enter 'a', to add a movie, 'l' to see your movie, 'f' to find a movie and 'q' to quit: ")
+
+    while user_input != 'q':
+        if user_input == 'a':
+            add_movie()
+        elif user_input == 'l':
+            show_movies(movies)
+        elif user_input == 'f':
+            find_movie()
+        else:
+            print('Unknown command - please try again.')
+
+        user_input = input("\nEnter 'a', to add a movie, 'l' to see your movie, 'f' to find a movie and 'q' to quit: ")
+
+
+def add_movie():
+
+    name = input("please provide movie title?: ")
+    director = input("please provide movie director?: ")
+    year = input("please provide movie release year?: ")
+    movies.append({
+        'name': name,
+        'director': director,
+        'year': year
+    })
+    print('The movies is now added to the movie database')
+
+
+def show_movies(movies_list):
+    for movie in movies_list:
+        show_movie_details(movie)
+        
+
+def show_movie_details(movie):
+    print(f"Name: {movie['name']}")
+    print(f"Director: {movie['director']}")
+    print(f"Release year: {movie['year']}")
+
+
+def find_movie():
+    find_by = input("What property of the movie are you looking for? ")
+    looking_for = input("What are you searching for? ")
+
+    found_movies = find_by_attribute(movies, looking_for, lambda x: x[find_by])
+
+    show_movies(found_movies)
+
+
+def find_by_attribute(items, expected, finder):
+  found = []
+
+  for i in items:
+    if finder(i) == expected:
+      found.append(i)
+
+  return found
+
+
+menu()
+
+
